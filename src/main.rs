142b58ca104ae1a41cfa1a02ebad639d233d5459
PeytonJohnson999 <Johnso630@stcharles.k12.la.us>
2024-10-20 04:53:06 +0000

    reformating for v1.0

--- src/main.rs
+++ src/main.rs
@@ -9,94 +9,114 @@ Code, Compile, Run and Debug online from anywhere in world.
 // #![feature(string_remove_matches)]
 
 use std::{
-    fs::{self, File}, 
-    io::{self, BufRead, BufReader, ErrorKind, Read, Write}, path::Display, str::FromStr //net::{TcpListener, TcpStream}
+    fs::{self, File},
+    io::{self, BufRead, BufReader, ErrorKind, Read, Write},
+    path::Display,
+    str::FromStr, //net::{TcpListener, TcpStream}
 };
 
 use anyhow::anyhow;
 use tokio::{
     // fs::{self, File},
-    io::{AsyncBufRead, AsyncReadExt, AsyncWrite, BufStream, AsyncWriteExt, AsyncBufReadExt, AsyncRead},
+    io::{
+        AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt,
+        BufStream,
+    },
     net::{TcpListener, TcpStream},
 };
 
-
-use resp::*;
 use req::*;
+use resp::*;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()>{
-
+async fn main() -> std::io::Result<()> {
     let mut port = 7878;
-    let listener: TcpListener = loop{
+    let listener: TcpListener = loop {
         match TcpListener::bind(format!("127.0.0.1:{port}")).await {
-            Ok(l) => {println!("chosen port: {port}"); break l},
+            Ok(l) => {
+                println!("chosen port: {port}");
+                break l;
+            }
             Err(e) => match e.kind() {
                 ErrorKind::AddrInUse => {
-                    if port == 65535{
+                    if port == 65535 {
                         port = 0;
                     }
                     port += 1;
                     continue;
-                },
+                }
                 _ => panic!("Error: {e:?}"),
-            }
+            },
         }
     };
-    
-    
-    loop{
+
+    loop {
         let (stream, _addr) = listener.accept().await?;
         let mut stream = BufStream::new(stream);
         println!("Connection Established!");
-        tokio::spawn(async move{
+        tokio::spawn(async move {
             let mut buf: Vec<u8> = Vec::new();
 
             loop {
-
-                let request = match parse_request(&mut stream).await{
+                let request = match parse_request(&mut stream).await {
                     Ok(r) => r,
                     Err(e) => {
                         // let mut req = String::new();
                         // stream.read_line(&mut req);
-                        panic!("Error parsing req, Root cause: {}",e.root_cause())},
+                        panic!("Error parsing req, Root cause: {}", e.root_cause())
+                    }
                 };
                 let method = request.method;
                 // println!("Path: {}", request.path);
-                
+
                 //Index & portfolio
-                if method == Method::GET && (request.path.trim() == "/" || request.path.trim() == "/index.html" ||
-                request.path.trim() == "/html/index.html"){
-
+                if method == Method::GET
+                    && (request.path.trim() == "/"
+                        || request.path.trim() == "/index.html"
+                        || request.path.trim() == "/html/index.html")
+                {
                     // println!("Index request");
-                    send_page("html/index.html", ContentType::html, &mut stream).await.unwrap();
-
-                } else if method == Method::GET && (request.path.trim() == "/portfolio.html" ||
-                request.path.trim() == "/html/portfolio.html"
-                ){
-                    send_page("html/portfolio.html", ContentType::html, &mut stream).await.unwrap();
+                    send_page("html/index.html", ContentType::html, &mut stream)
+                        .await
+                        .unwrap();
+                } else if method == Method::GET
+                    && (request.path.trim() == "/portfolio.html"
+                        || request.path.trim() == "/html/portfolio.html")
+                {
+                    send_page("html/portfolio.html", ContentType::html, &mut stream)
+                        .await
+                        .unwrap();
 
                 //Basic CSS
-                }else if method == Method::GET && request.path.trim() == "/css/style.css"{
-                    send_page("css/style.css", ContentType::css, &mut stream).await.unwrap();
-
-                }else if method == Method::GET{
+                } else if method == Method::GET && request.path.trim() == "/css/style.css" {
+                    send_page("css/style.css", ContentType::css, &mut stream)
+                        .await
+                        .unwrap();
+                } else if method == Method::GET {
                     let mut path = request.path.trim().to_string();
                     path.remove(0);
-                    match send_page(path.clone(), ContentType::from_str(&path).unwrap(), &mut stream).await {
+                    match send_page(
+                        path.clone(),
+                        ContentType::from_str(&path).unwrap(),
+                        &mut stream,
+                    )
+                    .await
+                    {
                         Ok(()) => (),
                         Err(e) => match e.root_cause().to_string().as_str() {
                             "No such file or directory (os error 2)" => {
-                                if path.contains("html"){
-                                    send_page("html/404.html", ContentType::html, &mut stream).await.unwrap()
+                                if path.contains("html") {
+                                    send_page("html/404.html", ContentType::html, &mut stream)
+                                        .await
+                                        .unwrap()
                                 }
-                            },
-                            _ => panic!("Error sending page: {e}")
+                            }
+                            _ => panic!("Error sending page: {e}"),
                         },
                     };
 
                 //404 html
-                }else if request.path.trim().contains(".html"){
+                } else if request.path.trim().contains(".html") {
                     eprintln!("404 error");
                     eprintln!("req: {request:?}");
                     let response = Response::err404Page();
@@ -104,8 +124,7 @@ async fn main() -> std::io::Result<()>{
                     response.write(&mut stream).await.unwrap();
 
                 //General 404
-                }else{
-
+                } else {
                     eprintln!("404 error");
                     eprintln!("req: {request:?}");
                     let response = Response::err404();
@@ -114,23 +133,26 @@ async fn main() -> std::io::Result<()>{
                 }
             }
         });
-            
     }
-    
+
     Ok(())
 }
 
-async fn send_page<S>(path: S , content_type: ContentType, stream: &mut BufStream<TcpStream>) -> Result<(), anyhow::Error>
+async fn send_page<S>(
+    path: S,
+    content_type: ContentType,
+    stream: &mut BufStream<TcpStream>,
+) -> Result<(), anyhow::Error>
 where
-    S: ToString
+    S: ToString,
 {
     let path = path.to_string();
-    let mut file = match fs::File::open(path.clone()){
+    let mut file = match fs::File::open(path.clone()) {
         Ok(f) => f,
         Err(e) => {
             eprintln!("Could not find file: {} while sending page", path);
-            return Err(e.into())
-        },
+            return Err(e.into());
+        }
     };
     let mut bytes = Vec::new();
     let len = file.read_to_end(&mut bytes).unwrap();
@@ -140,7 +162,7 @@ where
         "Content-Length".to_owned() => len.to_string(),
     };
 
-    let resp = Response{
+    let resp = Response {
         status: Status::Ok,
         headers,
         payload: io::Cursor::new(bytes),
@@ -151,29 +173,28 @@ where
     Ok(())
 }
 
-mod req{
-    use std::collections::HashMap;
+mod req {
     use anyhow::anyhow;
+    use std::collections::HashMap;
     use tokio::io::{AsyncBufRead, AsyncBufReadExt};
 
     #[derive(Debug, Clone)]
-    pub struct Request{
+    pub struct Request {
         pub method: Method,
         pub path: String,
         headers: HashMap<String, String>,
     }
 
     #[derive(PartialEq, Eq, Debug, Clone, Copy)]
-    pub enum Method{
+    pub enum Method {
         GET,
-
     }
 
-    impl TryFrom<&str> for Method{
+    impl TryFrom<&str> for Method {
         type Error = anyhow::Error;
 
         fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
-            match value{
+            match value {
                 "GET" => Ok(Method::GET),
                 m => Err(anyhow!("Invalid method: {m}")),
             }
@@ -188,10 +209,7 @@ mod req{
 
         let method: Method = parts
             .next()
-            .ok_or({
-                
-                anyhow::anyhow!("missing method in req: {}", line_buffer)
-            })
+            .ok_or({ anyhow::anyhow!("missing method in req: {}", line_buffer) })
             .and_then(TryInto::try_into)?;
 
         let path: String = parts
@@ -227,17 +245,17 @@ mod req{
     }
 }
 
-mod resp{
+mod resp {
     use std::{
-        collections::HashMap, fs::{self}, hash::Hash, io::{self, Bytes, Cursor, Read}, str::FromStr
+        collections::HashMap,
+        fs::{self},
+        hash::Hash,
+        io::{self, Bytes, Cursor, Read},
+        str::FromStr,
     };
 
     use anyhow::Ok;
-    use tokio::io::{
-        AsyncRead, 
-        AsyncWrite, 
-        AsyncWriteExt
-    };
+    use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
 
     #[derive(Debug, Clone)]
     pub struct Response<S: AsyncRead + Unpin> {
@@ -246,10 +264,8 @@ mod resp{
         pub payload: S,
     }
 
-    impl Response<io::Cursor<Vec<u8>>>{
-
-        pub fn favicon_resp() -> Self{
-
+    impl Response<io::Cursor<Vec<u8>>> {
+        pub fn favicon_resp() -> Self {
             let mut favicon = fs::File::open("images/favicon.ico").unwrap();
             let mut bytes = Vec::new();
             let len = favicon.read_to_end(&mut bytes).unwrap();
@@ -259,17 +275,18 @@ mod resp{
                 "Content-Length".to_owned() => len.to_string(),
             };
 
-            Self { 
-                status: Status::Ok, 
-                headers, 
-                payload: io::Cursor::new(bytes), 
+            Self {
+                status: Status::Ok,
+                headers,
+                payload: io::Cursor::new(bytes),
             }
         }
 
-        pub fn err404Page() -> Self{
-
+        pub fn err404Page() -> Self {
             let mut fBytes = Vec::new();
-            let len = fs::File::read_to_end(&mut fs::File::open("html/404.html").unwrap(), &mut fBytes).unwrap();
+            let len =
+                fs::File::read_to_end(&mut fs::File::open("html/404.html").unwrap(), &mut fBytes)
+                    .unwrap();
             let status = Status::NotFound;
 
             let headers = maplit::hashmap! {
@@ -277,32 +294,40 @@ mod resp{
                 "Content-Length".to_owned() => len.to_string(),
             };
 
-
-            Self{
+            Self {
                 status,
                 headers,
                 payload: io::Cursor::new(fBytes),
             }
         }
-        
-        pub fn err404() -> Self{
-            Self { status: Status::NotFound, headers: HashMap::new(), payload: Cursor::new( Vec::new()) }
+
+        pub fn err404() -> Self {
+            Self {
+                status: Status::NotFound,
+                headers: HashMap::new(),
+                payload: Cursor::new(Vec::new()),
+            }
         }
     }
 
-    impl<S: AsyncRead + Unpin> Response<S>{
+    impl<S: AsyncRead + Unpin> Response<S> {
         pub fn status_and_headers(&self) -> String {
-            let headers = self.headers
+            let headers = self
+                .headers
                 .iter()
-                .map(|(k,v)| format!("{k}: {v}"))
+                .map(|(k, v)| format!("{k}: {v}"))
                 .collect::<Vec<_>>()
-                .join("\r\n")
-                ;
+                .join("\r\n");
             format!("HTTP/1.1 {}\r\n{headers}\r\n\r\n", self.status)
         }
 
-        pub async fn write<O: AsyncWrite + Unpin>(mut self, stream: &mut O) -> Result<(), anyhow::Error>{
-            stream.write_all(self.status_and_headers().as_bytes()).await?;
+        pub async fn write<O: AsyncWrite + Unpin>(
+            mut self,
+            stream: &mut O,
+        ) -> Result<(), anyhow::Error> {
+            stream
+                .write_all(self.status_and_headers().as_bytes())
+                .await?;
 
             tokio::io::copy(&mut self.payload, stream).await?;
 
@@ -311,12 +336,12 @@ mod resp{
     }
 
     #[derive(PartialEq, Debug, Clone, Copy)]
-    pub enum Status{
+    pub enum Status {
         NotFound,
         Ok,
     }
 
-    impl std::fmt::Display for Status{
+    impl std::fmt::Display for Status {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             match self {
                 Self::NotFound => write!(f, "404 Not Found"),
@@ -326,21 +351,20 @@ mod resp{
     }
 
     #[derive(Debug)]
-    pub enum ContentType{
+    pub enum ContentType {
         js,
         html,
         css,
         jpg,
         png,
-        json, 
+        json,
         webp,
         ico,
     }
 
-    impl ToString for ContentType{
-
+    impl ToString for ContentType {
         fn to_string(&self) -> String {
-            match self{
+            match self {
                 ContentType::js => "text/javascript".to_owned(),
                 ContentType::html => "text/html".to_owned(),
                 ContentType::css => "text/css".to_owned(),
@@ -353,11 +377,10 @@ mod resp{
         }
     }
 
-    impl FromStr for ContentType{
+    impl FromStr for ContentType {
         type Err = anyhow::Error;
 
         fn from_str(s: &str) -> Result<Self, Self::Err> {
-            
             let pos = s.chars().count() - s.chars().rev().position(|c| c == '.').unwrap() - 1;
             let file_type = &s[pos..];
 
@@ -370,7 +393,7 @@ mod resp{
                 ".json" => Ok(ContentType::json),
                 ".webp" => Ok(ContentType::webp),
                 ".ico" => Ok(ContentType::ico),
-                _ => Err(anyhow::anyhow!("Invalid file type: {}", file_type))
+                _ => Err(anyhow::anyhow!("Invalid file type: {}", file_type)),
             }
         }
     }
